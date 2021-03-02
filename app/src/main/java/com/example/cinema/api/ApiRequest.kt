5b694014b9bbd4bca8a5f34a5b9710866975ae10
Andrey <dedok-iu3@yandex.ru>
2021-03-02 20:54:28 +0300

    cover

--- app/src/main/java/com/example/cinema/api/ApiRequest.kt
+++ app/src/main/java/com/example/cinema/api/ApiRequest.kt
@@ -1,14 +1,17 @@
 package com.example.cinema.api
 
 import com.example.cinema.models.Auth
+import com.example.cinema.models.Cover
 import kotlinx.coroutines.Deferred
 import retrofit2.Response
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 
 const val BASE_URL = "http://cinema.areas.su/"
+const val IMAGE_URL = "http://cinema.areas.su/up/images/"
 
 interface ApiRequest {
     @POST("auth/register")
@@ -18,4 +21,7 @@ interface ApiRequest {
     @POST("auth/login")
     @FormUrlEncoded
     suspend fun signin(@Field("email") email: String, @Field("password") password: String):Response<Auth>
+
+    @GET("movies/cover")
+    suspend fun getCover():Response<Cover>
 }
--- app/src/main/java/com/example/cinema/main/MainActivity.kt
+++ app/src/main/java/com/example/cinema/main/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.example.cinema.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.example.cinema.R
+import com.example.cinema.api.ApiRequest
+import com.example.cinema.api.BASE_URL
+import com.example.cinema.api.IMAGE_URL
+import com.example.cinema.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getCover()
+    }
+
+    private fun getCover(){
+        val api = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+                .create(ApiRequest::class.java)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val response = api.getCover()
+                withContext(Dispatchers.Main) {
+                    val backgroundImage = response.body()?.backgroundImage
+                    val foregroundImage = response.body()?.foregroundImage
+                    Glide.with(applicationContext).load(IMAGE_URL+backgroundImage).into(binding.background)
+                    Glide.with(applicationContext).load(IMAGE_URL+foregroundImage).into(binding.foreground)
+                }
+            }
+            catch (e: Exception){
+                Log.e("Main", "Error: ${e.message}")
+            }
+        }
     }
 }
