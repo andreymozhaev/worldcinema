28852253f62878aa9c626788ff4894c387c9e5df
Andrey <dedok-iu3@yandex.ru>
2021-03-02 11:13:02 +0300

    signin

--- app/src/main/java/com/example/cinema/api/ApiRequest.kt
+++ app/src/main/java/com/example/cinema/api/ApiRequest.kt
@@ -1,6 +1,7 @@
 package com.example.cinema.api
 
 import com.example.cinema.models.Auth
+import kotlinx.coroutines.Deferred
 import retrofit2.Response
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
--- app/src/main/java/com/example/cinema/signin/SigninActivity.kt
+++ app/src/main/java/com/example/cinema/signin/SigninActivity.kt
@@ -1,18 +1,24 @@
 package com.example.cinema.signin
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.example.cinema.R
 import com.example.cinema.api.ApiRequest
 import com.example.cinema.api.BASE_URL
 import com.example.cinema.databinding.ActivitySigninBinding
+import com.example.cinema.main.MainActivity
+import com.example.cinema.models.Auth
 import com.example.cinema.signup.SignupActivity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.lang.Exception
@@ -41,9 +47,15 @@ class SigninActivity : AppCompatActivity() {
         GlobalScope.launch(Dispatchers.IO) {
             try {
                 val response = api.signin(email, password)
-                Log.d("Main", "Response: ${response.body()}")
-                Log.d("Main", "Code: ${response.code()}")
-                Log.d("Main", "token: ${response.body()?.token.toString()}")
+                withContext(Dispatchers.Main) {
+                    val token = response.body()?.token
+                    if (token != null) {
+                        val intent = Intent(this@SigninActivity, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        val toast = Toast.makeText(this@SigninActivity, "Неавторизованный доступ", Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
             catch (e: Exception){
                 Log.e("Main", "Error: ${e.message}")
